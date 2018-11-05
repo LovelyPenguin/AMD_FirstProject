986c6badaed07e0ab8037fa634f548f45716dbf0
LovelyPenguin <lyp3238@gmail.com>
2018-11-05 16:33:12 +0900

    Add

---
+++ JenkinsUnityTestProject/Assets/Editor/ProjectBuilder.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+class ProjectBuilder
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static string APP_NAME = "YourProject";
+    static string TARGET_DIR = "target";
+
+    [MenuItem("Custom/CI/Build Windows")]
+    static void PerformWindows64()
+    {
+        string target_dir = APP_NAME + ".app";
+        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+        //string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+        //if (res.Length > 0)
+        //{
+        //    throw new Exception("BuildPlayer failure: " + res);
+        //}
+    }
+}
---
+++ JenkinsUnityTestProject/Assets/Scripts/BuildScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BuildScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
